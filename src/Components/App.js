2f7f2f719e61a3cf56ef04b5b8480b0d984d917d
Aditya Sharma <aditya.sharma.cms17@gmail.com>
2020-11-18 13:59:37 +0530

    ToDo-complete

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ToDoData from "./TodoData"
 import ToDoItem from "./ToDoItem"
-
+import "./components.css"
 class App extends React.Component{
     constructor()
     {
@@ -11,16 +11,38 @@ class App extends React.Component{
                 todo:ToDoData
             }
         }
+        this.handleChange=this.handleChange.bind(this)
+    }
+    handleChange(id)
+    {
+        this.setState(prevState => {
+            const updatedTodo=prevState.todo.map(todo => {
+                if(todo.id===id)
+                {
+                    //todo.completed=!todo.completed
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    }
+                }
+                return todo
+            })//map function closed
+            return{
+                todo:updatedTodo
+            }
+        })
+        console.log("changed",id)
     }
 
     render()
     {
         const todoItem = this.state.todo.map(
-            item=> <ToDoItem key={item.id}  item={item}/>    
+            item=> <ToDoItem key={item.id}  item={item}  handleChange={this.handleChange}/>    
         )//map closed
 
         return(
             <div>
+                
                 {todoItem}
             </div>
         )
