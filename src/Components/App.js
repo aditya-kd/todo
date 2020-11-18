a323c4b1628b97cf9e556934919f9ec33afa9983
Aditya Sharma <aditya.sharma.cms17@gmail.com>
2020-11-18 12:53:34 +0530

    click events tested

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,27 +1,42 @@
 import React from "react"
-import ToDoData from "./TodoData"
-import ToDoItem from "./ToDoItem"
-
+import "./components.css"
 class App extends React.Component{
     constructor()
     {
         super()
         {
             this.state={
-                todo:ToDoData
-            }
+                count:1}
+           
         }
+        this.handleClickDouble=this.handleClickDouble.bind(this)
+        this.handleClickHalf=this.handleClickHalf.bind(this)
+        }
+    
+    handleClickHalf()
+    {
+        this.setState((prevState)=>
+        {
+            return{
+                count: prevState.count/2
+            }
+        })
+    }
+    handleClickDouble()
+    {
+        this.setState((prevState)=>{
+            return{
+                count: prevState.count*2
+            }
+        })
     }
 
     render()
     {
-        const todoItem = this.state.todo.map(
-            item=> <ToDoItem key={item.id}  item={item}/>    
-        )//map closed
-
-        return(
+        return (
             <div>
-                {todoItem}
+                <h1>{this.state.count}</h1>
+                <button className="double_btn" onClick={this.handleClickDouble}>Double</button>   <button className="half_btn" onClick={this.handleClickHalf}>Half</button>   
             </div>
         )
     }
